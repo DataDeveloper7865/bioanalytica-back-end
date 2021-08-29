0c260e709fa5bfd6d74487951983d0644cb7b750
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-29 15:56:08 -0700

    comments to app

--- app.js
+++ app.js
@@ -13,12 +13,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Use the following utilites
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Require all routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
