36eef2ac94ff5c3096c946656e4e316fdec8f16b
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-02 22:47:23 -0700

    required dotenv for seeding

--- db/db.js
+++ db/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/bioanalytica`, {
     logging: false
