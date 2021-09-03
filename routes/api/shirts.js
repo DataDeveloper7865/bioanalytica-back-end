7c886a8f94619c1aadeba13dc53cbba7744892c6
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-02 22:46:52 -0700

    created get and create methods for shirts

---
+++ routes/api/shirts.js
@@ -0,0 +1,46 @@
+var router = require('express').Router();
+var { Shirt } = require('../../db/models/shirts')
+
+
+// Get all shirts
+router.get('/', async (req, res, next) => {
+    try {
+        const shirts = await Shirt.findAll();
+        return res.json({shirts})
+    } catch(err) {
+        return res.status(401);
+    }
+})
+
+// Get A Shirt By Id
+
+
+// Delete A Shirt By Id
+
+// Update A Shirt By Id
+
+//Create A New Shirt
+router.post('/new', async (req, res, next) => {
+    try {
+        const {shirtName, description, price, imgURL } = req.body;
+
+        console.log("MADE IT");
+        console.log("REQ.BODY IS", req.body);
+
+        if (!shirtName || !description || !price || !imgURL) {
+            return res
+            .status(400)
+            .json("ShirtName, Description, Price, and ImageURL are all required")
+        }
+
+        const shirt = await Shirt.create(req.body);
+
+        console.log("SHIRT IS ", shirt)
+
+        return res.json(shirt)
+    } catch(err) {
+        return res.status(401)
+    }
+})
+
+module.exports = router;
